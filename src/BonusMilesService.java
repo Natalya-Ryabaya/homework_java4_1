532386673673e580ead54e35df857d8e3d4f7b8e
Natalya Ryabaya <natalir.80@gmail.com>
2020-10-18 19:51:48 +0300

    modified files

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,5 +1,5 @@
 public class BonusMilesService {
-    public long calculate (int price){
+    public int calculate (int price){
         int mileOne = 20;
         int miles = price/mileOne;
         return miles;
